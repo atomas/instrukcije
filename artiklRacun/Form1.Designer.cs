b79155e8f0cf3890cb0256a47957de0ef3481d80
Anto Tomaš <atomas.maso@gmail.com>
2018-04-10 08:48:46 +0200

    unos artikala u listu ažuriranje listboxa s listom artikala

---
+++ artiklRacun/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace artiklRacun
+{
+    partial class artiklRacun
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKolicina = new System.Windows.Forms.TextBox();
+            this.txtCijena = new System.Windows.Forms.TextBox();
+            this.txtNaziv = new System.Windows.Forms.TextBox();
+            this.listaArtikli = new System.Windows.Forms.ListBox();
+            this.gumbUnos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(33, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Naziv";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(29, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cijena";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(19, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Količina";
+            // 
+            // txtKolicina
+            // 
+            this.txtKolicina.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtKolicina.Location = new System.Drawing.Point(82, 71);
+            this.txtKolicina.Name = "txtKolicina";
+            this.txtKolicina.Size = new System.Drawing.Size(100, 23);
+            this.txtKolicina.TabIndex = 3;
+            // 
+            // txtCijena
+            // 
+            this.txtCijena.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtCijena.Location = new System.Drawing.Point(82, 45);
+            this.txtCijena.Name = "txtCijena";
+            this.txtCijena.Size = new System.Drawing.Size(100, 23);
+            this.txtCijena.TabIndex = 4;
+            // 
+            // txtNaziv
+            // 
+            this.txtNaziv.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtNaziv.Location = new System.Drawing.Point(82, 19);
+            this.txtNaziv.Name = "txtNaziv";
+            this.txtNaziv.Size = new System.Drawing.Size(100, 23);
+            this.txtNaziv.TabIndex = 5;
+            // 
+            // listaArtikli
+            // 
+            this.listaArtikli.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listaArtikli.FormattingEnabled = true;
+            this.listaArtikli.ItemHeight = 15;
+            this.listaArtikli.Location = new System.Drawing.Point(12, 136);
+            this.listaArtikli.Name = "listaArtikli";
+            this.listaArtikli.Size = new System.Drawing.Size(265, 244);
+            this.listaArtikli.TabIndex = 6;
+            // 
+            // gumbUnos
+            // 
+            this.gumbUnos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.gumbUnos.Location = new System.Drawing.Point(82, 97);
+            this.gumbUnos.Name = "gumbUnos";
+            this.gumbUnos.Size = new System.Drawing.Size(100, 33);
+            this.gumbUnos.TabIndex = 7;
+            this.gumbUnos.Text = "Unesi artikl";
+            this.gumbUnos.UseVisualStyleBackColor = true;
+            this.gumbUnos.Click += new System.EventHandler(this.gumbUnos_Click);
+            // 
+            // artiklRacun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 399);
+            this.Controls.Add(this.gumbUnos);
+            this.Controls.Add(this.listaArtikli);
+            this.Controls.Add(this.txtNaziv);
+            this.Controls.Add(this.txtCijena);
+            this.Controls.Add(this.txtKolicina);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "artiklRacun";
+            this.Text = "Artikl Račun";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKolicina;
+        private System.Windows.Forms.TextBox txtCijena;
+        private System.Windows.Forms.TextBox txtNaziv;
+        private System.Windows.Forms.ListBox listaArtikli;
+        private System.Windows.Forms.Button gumbUnos;
+    }
+}
+
---
+++ artiklRacun/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace artiklRacun
+{
+    public class Artikl
+    {
+        public string naziv;
+        public int kolicina;
+        public int cijena;
+
+        public Artikl(string naz, int cij, int kol)
+        {
+            this.naziv = naz;
+            this.cijena = cij;
+            this.kolicina = kol;
+        }
+    }
+
+    public partial class artiklRacun : Form
+    {
+        public artiklRacun()
+        {
+            InitializeComponent();
+        }
+
+        List<Artikl> listaArtikala = new List<Artikl>();
+
+        private void gumbUnos_Click(object sender, EventArgs e)
+        {
+            string naziv = txtNaziv.Text;
+            int kolicina = Int32.Parse(txtKolicina.Text);
+            int cijena = Int32.Parse(txtCijena.Text);
+
+            Artikl artikl = new Artikl(naziv, cijena, kolicina);
+
+            /*ako u listi postoji artikl s tim nazivom */
+            if (listaArtikala.Exists(x => x.naziv == artikl.naziv))
+            {
+                azurirajArtikl(artikl);
+            }
+            else
+            {
+                listaArtikala.Add(artikl);
+            }
+            
+            /*nakon dodavanja ažurirati ispis na listBoxu*/
+            azuriranjeListeArtiakala(listaArtikala);
+
+            /*
+             PROUČI IMPLEMENTIRANE FUNKCIJE I LOGIKU KOJU SAM KORISTIO ZA RAD S LISTBOXOM S POPISOM ARTIKALA
+             IMPLEMENTIRAJ NASTAVAK APLIKACIJE KAKO GOD ZNAŠ I UMIJEŠ
+             KORISNIK IMA MOGUĆNOST ODABIRA ARTIKALA, TE KOLIČINU TOG ARTIKLA
+             KADA KLIKNE GUMB "DODAJ NA RAČUN" - LISTA ARTIKALA SMANJUJE KOLIČINU, A LISTA RAČUNA SE AŽURIRA (POVEĆANJE KOLIČINE ILI DODAVANJE NOVOG ARTIKLA)*/
+        }
+        
+        private void azurirajArtikl(Artikl artikl)
+        {
+            foreach (var art in listaArtikala)
+            {
+                /*
+                 ako već postoji artikl s tim nazivom
+                 povećaj količinu na skladištu
+                 ažuriraj cijenu artikla
+                 */
+                if (art.naziv == artikl.naziv)
+                {
+                    art.kolicina = art.kolicina + artikl.kolicina;
+                    art.cijena = artikl.cijena;
+                }
+            }
+            
+        }
+
+        private void azuriranjeListeArtiakala(List<Artikl> listaArtikala)
+        {
+            /*prvo ćemo obrisati list box i onda dodati ispočetka novi
+             kako bi bili sigurni da smo sve ažurirali*/
+            listaArtikli.Items.Clear();
+            foreach (var x in listaArtikala)
+            {
+                string upisUListu =
+                    "Naziv: " + x.naziv + "; Cijena: " + x.cijena + "; Količina: " + x.kolicina;
+                listaArtikli.Items.Add(upisUListu);
+            }
+        }
+    }
+}
