78b814cbf52d9edcdff486f6272ff8a93ee29292
Anto Tomaš <atomas.maso@gmail.com>
2018-04-10 09:01:30 +0200

    klasa artikl

---
+++ artiklRacun/Artikl.cs
@@ -0,0 +1,16 @@
+﻿namespace artiklRacun
+{
+    public class Artikl
+    {
+        public string naziv;
+        public int kolicina;
+        public int cijena;
+
+        public Artikl(string naz, int cij, int kol)
+        {
+            this.naziv = naz;
+            this.cijena = cij;
+            this.kolicina = kol;
+        }
+    }
+}
--- artiklRacun/Form1.cs
+++ artiklRacun/Form1.cs
@@ -10,20 +10,6 @@ using System.Windows.Forms;
 
 namespace artiklRacun
 {
-    public class Artikl
-    {
-        public string naziv;
-        public int kolicina;
-        public int cijena;
-
-        public Artikl(string naz, int cij, int kol)
-        {
-            this.naziv = naz;
-            this.cijena = cij;
-            this.kolicina = kol;
-        }
-    }
-
     public partial class artiklRacun : Form
     {
         public artiklRacun()
